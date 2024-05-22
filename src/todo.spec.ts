1d65e2e7179b51a4dcdd4b8015260ad804503c3c
Kar <kar.sobolewski@gmail.com>
2024-05-22 09:03:31 +0200

    Solution with bonus task

--- src/todo.spec.ts
+++ src/todo.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import inputJson from './input.json';
 import outputJson from './output.json';
-import { convertInput } from './todo';
+import { convertInput, validateOutput } from './todo';
 import { Input } from './types/input';
 
 describe('Todo', () => {
@@ -16,4 +16,8 @@ describe('Todo', () => {
   });
 
   // BONUS: Write tests that validates the output json. Use the function you have written in "src/todo.ts".
+  it('Should validate the output json', () => {
+    const output = convertInput(inputJson as Input);
+    expect(validateOutput(output)).to.equal(true);
+  });
 });
--- src/todo.ts
+++ src/todo.ts
@@ -1,5 +1,6 @@
-import { Annotation, Entity, Input } from './types/input';
+import { Annotation, Entity, EntityType, EntityClass, Input } from './types/input';
 import { ConvertedAnnotation, ConvertedEntity, Output } from './types/output';
+import * as yup from 'yup';
 
 // TODO: Convert Input to the Output structure. Do this in an efficient and generic way.
 // HINT: Make use of the helper library "lodash"
@@ -7,11 +8,17 @@ export const convertInput = (input: Input): Output => {
   const documents = input.documents.map((document) => {
     // TODO: map the entities to the new structure and sort them based on the property "name"
     // Make sure the nested children are also mapped and sorted
-    const entities = document.entities.map(convertEntity).sort(sortEntities);
+    const documentEntities = document.entities;
+    const entities = documentEntities.map(convertEntity(documentEntities)).sort(sortEntities);
 
     // TODO: map the annotations to the new structure and sort them based on the property "index"
     // Make sure the nested children are also mapped and sorted
-    const annotations = document.annotations.map(convertAnnotation).sort(sortAnnotations);
+    const documentAnnotations = document.annotations;
+    const annotations = documentAnnotations
+      .filter(filterAnnotations)
+      .map(convertAnnotation(entities, documentAnnotations))
+      .sort(sortAnnotations);
+
     return { id: document.id, entities, annotations };
   });
 
@@ -19,21 +26,103 @@ export const convertInput = (input: Input): Output => {
 };
 
 // HINT: you probably need to pass extra argument(s) to this function to make it performant.
-const convertEntity = (entity: Entity): ConvertedEntity => {
-  throw new Error('Not implemented');
+const convertEntity = (documentEntities: Entity[]) => (entity: Entity): ConvertedEntity => {
+  const children = documentEntities
+    .filter((e) => e.refs.includes(entity.id))
+    .map(convertEntity(documentEntities))
+    .sort(sortEntities);
+
+  return {
+    id: entity.id,
+    name: entity.name,
+    type: entity.type,
+    class: entity.class,
+    children,
+  }
 };
+
+const filterAnnotations = (annotation: Annotation) => !annotation.refs.length;
 
 // HINT: you probably need to pass extra argument(s) to this function to make it performant.
-const convertAnnotation = (annotation: Annotation): ConvertedAnnotation => {
-  throw new Error('Not implemented');
+const convertAnnotation = (entities: ConvertedEntity[], documentAnnotations: Annotation[]) => (annotation: Annotation): ConvertedAnnotation => {
+  const foundEntity = entities.find((e) => e.id === annotation.entityId) as ConvertedEntity;
+
+  const children = documentAnnotations
+    .filter((e) => e.refs.includes(annotation.id))
+    .map(convertAnnotation(entities, documentAnnotations))
+    .sort(sortAnnotations);
+
+  const getIndex = () => {
+    if (annotation.indices && annotation.indices.length) {
+      return annotation.indices[0].start;
+    }
+
+    if (children.length) {
+      return children[0].index;
+    }
+
+    return -1;
+  }
+
+  return {
+    id: annotation.id,
+    entity: {
+      id: foundEntity.id,
+      name: foundEntity.name,
+    },
+    value: annotation.value,
+    index: getIndex(),
+    children,
+  }
 };
 
-const sortEntities = (entityA: ConvertedEntity, entityB: ConvertedEntity) => {
-  throw new Error('Not implemented');
-};
+const sortElementsInAscendingOrder = (elementA: string | number, elementB: string | number): number => {
+  if (elementA > elementB) {
+    return 1;
+  } else if (elementA < elementB) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
 
-const sortAnnotations = (annotationA: ConvertedAnnotation, annotationB: ConvertedAnnotation) => {
-  throw new Error('Not implemented');
-};
+const sortEntities =
+  (entityA: ConvertedEntity, entityB: ConvertedEntity): number =>
+    sortElementsInAscendingOrder(entityA.name.toLowerCase(), entityB.name.toLowerCase());
+
+const sortAnnotations =
+  (annotationA: ConvertedAnnotation, annotationB: ConvertedAnnotation): number =>
+    sortElementsInAscendingOrder(annotationA.index, annotationB.index);
 
 // BONUS: Create validation function that validates the result of "convertInput". Use yup as library to validate your result.
+export const validateOutput = (output: Output): boolean => {
+  const entitySchema: yup.ObjectSchema<ConvertedEntity> = yup.object().shape({
+    id: yup.string().required(),
+    name: yup.string().required(),
+    type: yup.string<EntityType>().required(),
+    class: yup.string<EntityClass>().required(),
+    children: yup.array().of(yup.lazy(() => entitySchema)).required(),
+  });
+
+  const annotationSchema: yup.ObjectSchema<ConvertedAnnotation> = yup.object().shape({
+    id: yup.string().required(),
+    entity: yup.object().shape({
+      id: yup.string().required(),
+      name: yup.string().required(),
+    }).required(),
+    value: yup.mixed<string | number>().required().nullable(),
+    index: yup.number().required(),
+    children: yup.array().of(yup.lazy(() => annotationSchema)).required(),
+  });
+
+  const outputSchema = yup.object().shape({
+    documents: yup.array()
+      .of(yup.object().shape({
+        id: yup.string(),
+        entities: yup.array().of(entitySchema),
+        annotations: yup.array().of(annotationSchema),
+      })),
+  });
+
+  return outputSchema.isValidSync(output);
+}
